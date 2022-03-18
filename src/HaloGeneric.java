923dd9195a6e292265af88e38810bdd91476da05
Ahmadnaufal2525 <ahmadnaufal588@gmail.com>
2022-03-18 14:37:02 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ src/HaloGeneric.java
@@ -0,0 +1,9 @@
+public class HaloGeneric {
+    public static void SapaUser(String X) {
+        System.out.printf("Halo user %s",X);
+    }
+
+    public static void main(String[] args) {
+        SapaUser("Naufal");
+    }
+}
